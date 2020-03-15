173a12bd9a43ac6d621679fd9dbff22b91f42382
Joe Patton <umadjoe@gmail.com>
2020-03-15 15:49:12 -0400

    getting flask to work

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/happiness.sqlite"
 db = SQLAlchemy(app)
 
 # reflect an existing database into a new model
@@ -27,8 +27,8 @@ Base = automap_base()
 Base.prepare(db.engine, reflect=True)
 
 # Save references to each table
-Samples_Metadata = Base.classes.sample_metadata
-Samples = Base.classes.samples
+
+Freedom_short = Base.classes.freedom_idx_2019
 
 
 @app.route("/")
@@ -37,69 +37,46 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/names")
+@app.route("/freedom_columns")
 def names():
     """Return a list of sample names."""
 
     # Use Pandas to perform the sql query
-    stmt = db.session.query(Samples).statement
+    stmt = db.session.query(Freedom_short).statement
     df = pd.read_sql_query(stmt, db.session.bind)
 
     # Return a list of the column names (sample names)
     return jsonify(list(df.columns)[2:])
 
 
-@app.route("/metadata/<sample>")
-def sample_metadata(sample):
+@app.route("/metadata/<iso_code>")
+def sample_metadata(iso_code):
     """Return the MetaData for a given sample."""
     sel = [
-        Samples_Metadata.sample,
-        Samples_Metadata.ETHNICITY,
-        Samples_Metadata.GENDER,
-        Samples_Metadata.AGE,
-        Samples_Metadata.LOCATION,
-        Samples_Metadata.BBTYPE,
-        Samples_Metadata.WFREQ,
+        Freedom_short.iso_code,
+        Freedom_short.year,
+        Freedom_short.country,
+        Freedom_short.region,
+        Freedom_short.hf_score,
+        Freedom_short.hf_rank,
+        Freedom_short.hf_quartile,
     ]
 
-    results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
+    results = db.session.query(*sel).filter(Freedom_short.iso_code == iso_code).all()
 
     # Create a dictionary entry for each row of metadata information
-    sample_metadata = {}
+    freedom_sdata = {}
     for result in results:
-        sample_metadata["sample"] = result[0]
-        sample_metadata["ETHNICITY"] = result[1]
-        sample_metadata["GENDER"] = result[2]
-        sample_metadata["AGE"] = result[3]
-        sample_metadata["LOCATION"] = result[4]
-        sample_metadata["BBTYPE"] = result[5]
-        sample_metadata["WFREQ"] = result[6]
+        freedom_sdata["iso_code"] = result[0]
+        freedom_sdata["year"] = result[1]
+        freedom_sdata["country"] = result[2]
+        freedom_sdata["region"] = result[3]
+        freedom_sdata["hf_score"] = result[4]
+        freedom_sdata["hf_rank"] = result[5]
+        freedom_sdata["hf_quartile"] = result[6]
 
-    print(sample_metadata)
-    return jsonify(sample_metadata)
-
-
-@app.route("/samples/<sample>")
-def samples(sample):
-    """Return `otu_ids`, `otu_labels`,and `sample_values`."""
-    stmt = db.session.query(Samples).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
-
-    # Filter the data based on the sample number and
-    # only keep rows with values above 1
-    sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
-
-    # Sort by sample
-    sample_data.sort_values(by=sample, ascending=False, inplace=True)
-
-    # Format the data to send as json
-    data = {
-        "otu_ids": sample_data.otu_id.values.tolist(),
-        "sample_values": sample_data[sample].values.tolist(),
-        "otu_labels": sample_data.otu_label.tolist(),
-    }
-    return jsonify(data)
-
+    print(freedom_sdata)
+    return jsonify(freedom_sdata)
 
 if __name__ == "__main__":
     app.run()
