fbbf3525819351505a9d013a9b35645f7804bcfc
Joe Patton <umadjoe@gmail.com>
2020-03-18 15:01:49 -0400

    panel is displaying data2

--- static/js/app.js
+++ static/js/app.js
@@ -15,6 +15,33 @@ function buildMetadata(iso_code) {
       para.text(key_value);
     })
   
+    var gauge_data = [
+      {
+        domain: { x: [0, 1], y: [0, 1] },
+        value: data.hf_score,
+        title: { text: "Happiness score" },
+        type: "indicator",
+        mode: "gauge+number+delta",
+        gauge: {
+          axis: { range: [0, 9] },
+          steps: [
+            { range: [0, 1], color: "red" },
+            { range: [1, 2], color: "red" },
+            { range: [2, 3], color: "red" },
+            { range: [3, 4], color: "yellow" },
+            { range: [4, 5], color: "yellow" },
+            { range: [5, 6], color: "yellow" },
+            { range: [6, 7], color: "lightgreen" },
+            { range: [7, 8], color: "lightgreen" },
+            { range: [8, 9], color: "lightgreen" }
+          ],
+        }
+      }
+    ];
+    
+    var gauge_layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+    Plotly.newPlot('gauge', gauge_data, gauge_layout);
+
   });  
 
   
@@ -53,5 +80,11 @@ function init() {
     buildMetadata(newSample);
   }
   
+  function optionChanged(newSample) {
+    // Fetch new data each time a new sample is selected
+    buildCharts(newSample);
+    buildMetadata(newSample);
+  }
+
   // Initialize the dashboard
   init();
